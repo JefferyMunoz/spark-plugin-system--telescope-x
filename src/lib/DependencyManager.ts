75059e8781ec859fc68ad8fd40415235fc11a071
JefferyMunoz <jefferysong@foxmail.com>
2026-01-26 21:22:18 +0800

    fix: 修复 getAllPorts 语法错误

--- src/lib/DependencyManager.ts
+++ src/lib/DependencyManager.ts
@@ -1,6 +1,6 @@
 import { ipcRenderer } from 'electron';
 
-const getAllPorts: () => {
+const getAllPorts = () => {
   console.log('[DependencyManager] getAllPorts called');
   // 调用 spark-master 的 getAllPorts API 获取真实的端口数据
   return ipcRenderer.invoke('msg-trigger-async', { type: 'getAllPorts' });
